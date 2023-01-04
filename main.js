b4e7da925e19ff7fed2265f0fb79e6d95210fd84
RadimYassin <100171335+RadimYassin@users.noreply.github.com>
2023-01-04 22:49:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 
-let scoreInDom=document.getElementById('score')
 
+let scoreInDom=document.getElementById('score')
+let count =0
 let numberInDOM=document.getElementById("number")
 setInterval(()=>{
  
     let numerRandom=Math.floor(Math.random()*100)
     numberInDOM.innerHTML=numerRandom
+    count++
+    if(count>10){
+        alert("your score : "+scoreInDom.innerHTML+"/10")
+        count=0;
+        scoreInDom.innerHTML=0
+        numberInDOM.innerHTML=""
+    }
     
-},5000)
+},500)
 
 const CheckNumber=()=>{
     
@@ -26,9 +34,7 @@ pair.addEventListener('click',()=>{
     if(CheckNumber()==1){
         scoreInDom.innerHTML=+scoreInDom.innerHTML+1
     }
-    if ( scoreInDom.innerHTML>10) {
-        scoreInDom.innerHTML=0
-    }
+   
 })
 
 impair.addEventListener('click',()=>{
@@ -36,11 +42,12 @@ impair.addEventListener('click',()=>{
     if(CheckNumber()==0){
         scoreInDom.innerHTML=+scoreInDom.innerHTML+1
     } 
-    if ( scoreInDom.innerHTML>10) {
-        scoreInDom.innerHTML=0
-    }
+    
     
 })
+
+
+
 
 
 
